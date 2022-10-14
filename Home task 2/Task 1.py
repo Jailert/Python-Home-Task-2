0ebfe9356c135ec5039cf3c6ab4924a33bfd3602
unknown <antonjailert@gmail.com>
2022-10-15 00:32:40 +0300

    Completed

--- Home task 2/Task 1.py
+++ Home task 2/Task 1.py
@@ -1,28 +1,23 @@
 # Напишите программу, которая принимает на вход вещественное число
 # и показывает сумму его цифр.
 
-
 number = input('Enter number: ')
+
+
+def digitsum(n):
+    sum = 0
+    while n != 0:
+        sum = sum + (n % 10)
+        n = n // 10
+    return sum
+
 
 if '.' in number:
     splitnumber = number.split(".")
 
     whole = int(splitnumber[0])
     fractional = int(splitnumber[1])
-
-    sum = 0
-
-    while (whole != 0):
-        sum = sum + (whole % 10)
-        whole = whole // 10
-
-    while (fractional != 0):
-        sum = sum + (fractional % 10)
-        fractional = fractional // 10
+    print(f' Sum :{(digitsum(whole) + digitsum(fractional))}')
 else:
     number = int(number)
-    sum = 0
-    while number > 0:
-        sum = sum + (number % 10)
-        number = number // 10
-print(sum)
+    print(f' Sum : {digitsum(number)}')
